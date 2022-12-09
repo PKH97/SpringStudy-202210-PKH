e5fda9ccc1335a114ff5def7876d1fc4aba815b4
K.Hyo park <116334234+PKH97@users.noreply.github.com>
2022-12-09 21:48:50 +0900

    Add files via upload
    22.12.19 스프링부트의 MVC

--- src/main/java/com/study/springboot202210PKH/locAndDi/StudentController.java
+++ src/main/java/com/study/springboot202210PKH/locAndDi/StudentController.java
@@ -29,5 +29,4 @@ public class StudentController {
 
         return null;
     }
-
 }
--- src/main/java/com/study/springboot202210PKH/web/controller/Controller1.java
+++ src/main/java/com/study/springboot202210PKH/web/controller/Controller1.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Controller1 {
 
 //    @RequestMapping(value = "/page1", method = RequestMethod.GET)
-    @GetMapping
-    public  String page1(){
-        return "page1";
+    @GetMapping("/page1")
+    public ModelAndView page1(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("page1");
+        mav.addObject("key", "value");
+        return mav;
     }
+    // 위와 아래는 같다.
+//    public  String page1(){
+//        return "page1";
+//    }
 
     @GetMapping("page2")
     public  String page2(){
