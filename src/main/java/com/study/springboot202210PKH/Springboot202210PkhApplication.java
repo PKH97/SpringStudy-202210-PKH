45c8075a2f47d1ac68f734764850bbb780a178f1
K.Hyo park <116334234+PKH97@users.noreply.github.com>
2022-12-12 21:53:11 +0900

    Add files via upload

--- src/main/java/com/study/springboot202210PKH/Springboot202210PkhApplication.java
+++ src/main/java/com/study/springboot202210PKH/Springboot202210PkhApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot202210PkhApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot202210PkhApplication.class, args);
 	}
-
 }
